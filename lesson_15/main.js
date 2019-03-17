9806fa974d3a19b5348bada91e28f9ca6bc6c43c
Liliiia <lilya_dikaya@ukr.net>
2019-03-17 21:43:34 +0200

    lesson_15

---
+++ lesson_15/main.js
@@ -0,0 +1,46 @@
+var age = 25, name = 'Diana';
+console.log(age, name); 
+
+var num= 1 > 3;
+console.log(num);
+
+var array = [1, 2, 3, 4, 5];
+console.log(array);
+
+var obj = {text: 'Hello'};
+console.log(obj);
+
+var num = 2e5;
+console.log(num.toFixed(2));
+
+var i = 100;
+console.log(++i);
+console.log(i);
+var i = 49;
+console.log(i++);
+console.log(i);
+
+var n = 50;
+n += 50;
+console.log(n);
+n -= 40;
+console.log(n);
+n *= 2;
+console.log(n);
+n /= 2;
+console.log(n);
+n %= 25;
+console.log(n);
+
+
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random()*10);
+console.log(Math.pow(2,5));
+
+var string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, deserunt';
+console.log(string.length);
+console.log(string.substring(6,12));
+console.log(string.replace('ipsum', 'dolor'));
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
